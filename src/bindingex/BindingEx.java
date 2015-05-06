d4c956947a6fd3f3192346dbc8125e8714971e5c
geirfn <geir.nygard@bekk.no>
2015-05-06 11:57:27 +0200

    First commit

---
+++ src/bindingex/BindingEx.java
@@ -0,0 +1,28 @@
+package bindingex;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class BindingEx extends Application {
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        stage.setTitle("Form Sample");
+
+        Parent root = FXMLLoader.load(getClass().getResource("Form.xml"));
+        Scene scene = new Scene(root, 600, 300);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/bindingex/FormController.java
@@ -0,0 +1,26 @@
+package bindingex;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class FormController implements Initializable {
+
+    @FXML
+    private TextField plusField1;
+    @FXML
+    private TextField plusField12;
+
+    @FXML
+    private Label result;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }
+}
